fd48a17acf7162b8640137ce1e507ef466a01773
Michael Hart <hart.mike@gmail.com>
2024-02-22 12:57:47 -0500

    Add ecr_private_repo module

---
+++ modules/aws/ecr_private_repo/outputs.tf
@@ -0,0 +1,19 @@
+output "repository_arn" {
+  description = "Repository ARN"
+  value       = aws_ecr_repository.repo.arn
+}
+
+output "repository_url" {
+  description = "Repository URL"
+  value       = aws_ecr_repository.repo.repository_url
+}
+
+output "github_iam_role_arn" {
+  description = "GitHub OIDC IAM Role ARN"
+  value       = aws_iam_role.github_actions.arn
+}
+
+output "github_iam_role_name" {
+  description = "GitHub OIDC IAM Role Name"
+  value       = aws_iam_role.github_actions.name
+}
