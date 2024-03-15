aeba996b10002406f0eef9bcb2f5de34fdc56e12
Michael Hart <hart.mike@gmail.com>
2024-03-15 12:58:11 -0400

    Add checkov tests

--- modules/aws/cloudwatchlogs/main.tf
+++ modules/aws/cloudwatchlogs/main.tf
@@ -6,6 +6,8 @@ locals {
 }
 
 resource "aws_cloudwatch_log_group" "default" {
+  #checkov:skip=CKV_AWS_158:False positive, KMS key is defined and required.connection
+  #checkov:skip=CKV_AWS_338:Logs should be kept for a year except this doesn't make sense in dev/test environments.
   name              = var.path
   retention_in_days = var.retention_in_days
   kms_key_id        = length(var.kms_key_arn) == 0 ? aws_kms_key.cwl[0].arn : var.kms_key_arn
