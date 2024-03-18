e8fde53f5f1ec59b1b2c78d9fab9fbed47c2db8e
Michael Hart <hart.mike@gmail.com>
2024-03-18 11:01:42 -0400

    Rework the KMS part

--- modules/aws/cloudwatchlogs/main.tf
+++ modules/aws/cloudwatchlogs/main.tf
@@ -3,6 +3,7 @@ locals {
     var.default_tags,
     tomap({ "environment" = var.environment, "component" = var.name, "application" = var.name }),
   )
+  kms_alias_arn = "arn:aws:kms:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${var.cwl_kms_alias_name}"
 }
 
 resource "aws_cloudwatch_log_group" "default" {
@@ -10,7 +11,7 @@ resource "aws_cloudwatch_log_group" "default" {
   #checkov:skip=CKV_AWS_338:Logs should be kept for a year except this doesn't make sense in dev/test environments.
   name              = var.path
   retention_in_days = var.retention_in_days
-  kms_key_id        = length(var.kms_key_arn) == 0 ? aws_kms_key.cwl[0].arn : var.kms_key_arn
+  kms_key_id        = local.kms_alias_arn
   tags = merge(
     local.tags,
     {
--- modules/aws/cloudwatchlogs/variables.tf
+++ modules/aws/cloudwatchlogs/variables.tf
@@ -16,6 +16,7 @@ variable "path" {
   }
 }
 
+# The retention periods are set by AWS.
 variable "retention_in_days" {
   description = <<EOF
     The number of days to retain the log events in the log group. Valid values are: 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, and 3653.
@@ -26,14 +27,6 @@ variable "retention_in_days" {
     condition     = contains([1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1096, 1827, 2192, 2557, 2922, 3288, 3653], var.retention_in_days)
     error_message = "Cloudwatch Logs retention days must be one of 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1096, 1827, 2192, 2557, 2922, 3288, 3653."
   }
-}
-
-variable "kms_key_arn" {
-  description = <<EOF
-    The ARN of the KMS key to use when encrypting log data. If not specified a new KMS key will be generated.
-    EOF
-  type        = string
-  default     = ""
 }
 
 variable "environment" {
@@ -49,4 +42,10 @@ variable "default_tags" {
     "owner"      = "Engineering"
     "managed-by" = "terraform"
   }
+}
+
+variable "cwl_kms_alias_name" {
+  description = "The alias name of the KMS key used to encrypt the CloudWatch Log Group. The full ARN will be constructed using the current region and account id."
+  type        = string
+  default     = "alias/apres/cloudwatchlogs"
 }
