66fe133f70781787431cfc2135dddc3a8c118447
Michael Hart <hart.mike@gmail.com>
2024-07-29 12:02:41 -0400

    Fix output when api_gateway_logging is not enabled (#136)
    * Fix output when api_gateway_logging is not enabled
    
    * Update cloudwatchlogs_regional CHANGELOG
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- modules/aws/cloudwatchlogs_regional/CHANGELOG.md
+++ modules/aws/cloudwatchlogs_regional/CHANGELOG.md
@@ -2,6 +2,10 @@
 
 This change log is automatically generated.
 
+## 1.2.0 - 2024-07-29
+
+Fix output when enable_api_gateway_logging is set to false.
+
 ## 1.1.0 - 2024-07-09
 
 Add IAM role and API Gateway configuration to enable CloudWatch Logs logging.
--- modules/aws/cloudwatchlogs_regional/outputs.tf
+++ modules/aws/cloudwatchlogs_regional/outputs.tf
@@ -9,5 +9,6 @@ output "kms_alias" {
 }
 
 output "api_gateway_cloudwatch_logs_role_arn" {
-  value = aws_iam_role.apigw_cwl[0].arn
+  description = "ARN of the API Gateway CloudWatch Logs role, or empty string if enable_api_gateway_logging = false"
+  value       = var.enable_api_gateway_logging ? aws_iam_role.apigw_cwl[0].arn : ""
 }
