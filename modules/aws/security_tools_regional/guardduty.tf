038461dcbade7987c6dfdc837c8c40df48b308db
Michael Hart <hart.mike@gmail.com>
2024-08-12 18:37:22 +0000

    Switch to creating the detector instead of looking it up.

--- modules/aws/security_tools_regional/guardduty.tf
+++ modules/aws/security_tools_regional/guardduty.tf
@@ -1,14 +1,25 @@
-# Reference the detector created by AWS Organizations.
-data "aws_guardduty_detector" "default" {}
+# Create the AWS GuardDuty detector and enable the features for the detector.
+#
+# NOTE: It's unclear how and when, but sometimes AWS Organizations creates the detector,
+# in which case you will have to do an import of the detector, see the README for details.
+resource "aws_guardduty_detector" "default" {
+  enable = true
+  tags = merge(
+    local.tags,
+    {
+      Name = "SecurityHubCriticalHighFindings"
+    },
+  )
+}
 
 resource "aws_guardduty_organization_configuration" "default" {
   auto_enable_organization_members = "ALL"
-  detector_id                      = data.aws_guardduty_detector.default.id
+  detector_id                      = aws_guardduty_detector.default.id
 }
 
 resource "aws_guardduty_detector_feature" "eks_runtime_monitoring" {
   count       = var.guardduty_enable_eks_protection ? 1 : 0
-  detector_id = data.aws_guardduty_detector.default.id
+  detector_id = aws_guardduty_detector.default.id
   name        = "EKS_RUNTIME_MONITORING"
   status      = "ENABLED"
   additional_configuration {
@@ -19,21 +30,21 @@ resource "aws_guardduty_detector_feature" "eks_runtime_monitoring" {
 
 resource "aws_guardduty_detector_feature" "s3" {
   count       = var.guardduty_enable_s3_protection ? 1 : 0
-  detector_id = data.aws_guardduty_detector.default.id
+  detector_id = aws_guardduty_detector.default.id
   name        = "S3_DATA_EVENTS"
   status      = "ENABLED"
 }
 
 resource "aws_guardduty_detector_feature" "rds" {
   count       = var.guardduty_enable_rds_protection ? 1 : 0
-  detector_id = data.aws_guardduty_detector.default.id
+  detector_id = aws_guardduty_detector.default.id
   name        = "RDS_LOGIN_EVENTS"
   status      = "ENABLED"
 }
 
 resource "aws_guardduty_detector_feature" "lambda" {
   count       = var.guardduty_enable_lambda_protection ? 1 : 0
-  detector_id = data.aws_guardduty_detector.default.id
+  detector_id = aws_guardduty_detector.default.id
   name        = "LAMBDA_NETWORK_LOGS"
   status      = "ENABLED"
 }
