ca3a6f09e93003bf4934f932b577f5c031d4edd4
Edward Dean <emdean5183@gmail.com>
2024-08-14 13:52:41 -0400

    - Saving the code signing config ID in a re-usable SSM parameter to allow lambda modules to look this up at deploy time.

--- modules/aws/lambda_regional/main.tf
+++ modules/aws/lambda_regional/main.tf
@@ -25,3 +25,30 @@ resource "aws_lambda_code_signing_config" "default" {
 
 }
 
+# Save the lambda signing config arn in a well known parameter.  This is needed by the lambda module to re-use the same signing 
+# configuration across lambda functions, rather than creating a configuration per function.
+#
+# We need the parameter because the only way to lookup a data.aws_lambda_code_signing_config is with the arn and a portion of the arb is
+# auto-generated after the terraform completes (i.e.)
+
+# data "aws_lambda_code_signing_config" "existing_csc" {
+#  arn = "arn:aws:lambda:${var.aws_region}:${var.aws_account}:code-signing-config:csc-0f6c334abcdea4d8b"
+#}
+#
+# The id csc-0f6c334abcdea4d8b is completely random, as such there is no way to look this up based on convention.  So we need a paramter to 
+# store and retrieve this at deploy time.
+resource "aws_ssm_parameter" "default" {
+  name        = "apres/lambda/lambda-signing-config-arn"
+  description = "The ARN for the lambda signing configuration."
+  type        = "SecureString"
+  value       = aws_lambda_code_signing_config.default.arn
+
+  #checkov:skip=CKV_AWS_337:"Ensure SSM parameters are using KMS CMK"
+
+  tags = merge(
+    local.tags,
+    tomap({
+      Name = "apres/lambda/lambda-signing-config-arn"
+    })
+  )
+}
