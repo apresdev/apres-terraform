5422fefc2b208c2c6f4524d54d0f444d0edb0a68
emdean <47194768+emdean@users.noreply.github.com>
2024-08-15 09:00:31 -0400

    Save the signing profile name in a well known parameter (#172)
    * - Save the signing profile name in a well known parameter
    
    * Update lambda_regional CHANGELOG
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- modules/aws/lambda_regional/main.tf
+++ modules/aws/lambda_regional/main.tf
@@ -28,20 +28,50 @@ resource "aws_lambda_code_signing_config" "default" {
 # Save the lambda signing config arn in a well known parameter.  This is needed by the lambda module to re-use the same signing 
 # configuration across lambda functions, rather than creating a configuration per function.
 #
-# We need the parameter because the only way to lookup a data.aws_lambda_code_signing_config is with the arn and a portion of the arb is
+# We need the parameter because the only way to lookup a data.aws_lambda_code_signing_config is with the arn and a portion of the arn is
 # auto-generated after the terraform completes (i.e.)
 
-# data "aws_lambda_code_signing_config" "existing_csc" {
+# data "aws_lambda_code_signing_config" "existing" {
 #  arn = "arn:aws:lambda:${var.aws_region}:${var.aws_account}:code-signing-config:csc-0f6c334abcdea4d8b"
 #}
 #
-# The id csc-0f6c334abcdea4d8b is completely random, as such there is no way to look this up based on convention.  So we need a paramter to 
-# store and retrieve this at deploy time.
-resource "aws_ssm_parameter" "default" {
+# The id csc-0f6c334abcdea4d8b is completely random, as such there is no way to look this up based on convention.  So we need a parameter to
+# store and retrieve this at deploy time, as the lambda module needs the code signing configuration to ensure that the lambda function only uses
+# functions signed using the given configuration.
+resource "aws_ssm_parameter" "signing_config_arn" {
   name        = "/apres/lambda/signing-config-arn"
   description = "The ARN for the lambda signing configuration."
   type        = "SecureString"
   value       = aws_lambda_code_signing_config.default.arn
+
+  #checkov:skip=CKV_AWS_337:"Ensure SSM parameters are using KMS CMK"
+
+  tags = merge(
+    local.tags,
+    tomap({
+      Name = "/apres/lambda/signing-config-arn"
+    })
+  )
+}
+
+# Save the lambda signing profile name in a well known parameter.  This is needed by the lambda module to re-use the same signing
+# profile across lambda functions, rather than creating a signing profile per function.
+#
+# We need the parameter because the only way to lookup a data.aws_signer_signing_profile is with the profile name and a portion of the name is
+# auto-generated after the terraform completes (i.e.)
+
+# data "aws_signer_signing_profile" "existing" {
+#  name = "ApresLambdaSigningProfile_DdW3Mk1foYL88fajut4mTVFGpuwfd4ACO6ANL0D1uIj7lrn8adK"
+#}
+#
+# The name suffix DdW3Mk1foYL88fajut4mTVFGpuwfd4ACO6ANL0D1uIj7lrn8adK is completely random, as such there is no way to look this up based on
+# convention.  So we need a parameter to store and retrieve this at deploy time, as the lambda module needs the signing profile name when creating
+# a signing job.
+resource "aws_ssm_parameter" "signing_profile_name" {
+  name        = "/apres/lambda/signing-profile-name"
+  description = "The name of the signing profile for the lambda signing configuration."
+  type        = "SecureString"
+  value       = aws_signer_signing_profile.default.name
 
   #checkov:skip=CKV_AWS_337:"Ensure SSM parameters are using KMS CMK"
 
