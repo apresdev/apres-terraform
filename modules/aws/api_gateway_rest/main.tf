ba697a8c7d69f849cd02cff0b6e1b5501c97b010
Michael Hart <hart.mike@gmail.com>
2024-10-24 21:20:40 +0000

    Update names to reflect naming standards

--- modules/aws/api_gateway_rest/main.tf
+++ modules/aws/api_gateway_rest/main.tf
@@ -10,3 +10,10 @@ locals {
     })
   )
 }
+
+module "apres_names" {
+  #checkov:skip=CKV_TF_1:False positive, we are not using a hash because we use the tagged version.
+  source      = "git@github.com:apresdev/apres-terraform.git//modules/aws/apres_names?ref=rel/apres_names/1.0.0"
+  name        = var.name
+  environment = var.environment
+}
