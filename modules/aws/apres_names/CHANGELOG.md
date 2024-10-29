a91a648133a76445e9a28f34fa9365ee9b6a9cb0
Michael Hart <hart.mike@gmail.com>
2024-10-29 14:56:46 -0400

    Add an apres_names module (#246)
    * Add an apres-naming module
    
    * Update global docs with apres-names
    
    * Update apres_names CHANGELOG
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ modules/aws/apres_names/CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+This change log is automatically generated.
+
+## 1.0.0 - 2024-10-29
+
+Add the apres_names module.
---
+++ modules/aws/apres_names/outputs.tf
@@ -0,0 +1,9 @@
+output "local_name" {
+  description = "The local name"
+  value       = local.local_name
+}
+
+output "global_name" {
+  description = "The global name of the resource"
+  value       = local.global_name
+}
