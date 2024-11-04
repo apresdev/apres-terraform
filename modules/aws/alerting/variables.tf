5e616b3ca1c59615688e2cf17ad96ebeaff29250
Michael Hart <hart.mike@gmail.com>
2024-11-04 11:22:42 -0500

    Rewrite the Alerting module to handle multiple regions (#263)
    * Rewrite the Alerting module to handle multiple regions
    
    * Add testing comment and empty test case
    
    * Fixes from code review
    
    * Update alerting CHANGELOG
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- modules/aws/alerting/variables.tf
+++ modules/aws/alerting/variables.tf
@@ -63,31 +63,10 @@ variable "slack_workspace_id" {
   default     = ""
 }
 
-variable "slack_channel_id" {
-  description = <<EOF
-  The Slack channel ID for notifications. To find a channel ID, in Slack, right click on a channel
-  and select "View channel details" and the Channel ID should be at the bottom, like C07S3JC2C0N
-  EOF
-  type        = string
-  default     = ""
-}
-
 variable "msteams_team_id" {
   description = <<EOF
     The Microsoft Teams "Team ID" for notifications. This is displayed in the AWS Console. If not set,
     Teams integration will not be enabled.
-    EOF
-  type        = string
-  default     = ""
-}
-
-variable "msteams_channel_id" {
-  description = <<EOF
-    The Microsoft Teams Channel ID for nofications.  The Channel Id is buried in the URL to the channel,
-    and can be found in Teams using the "Get link to channel" menu option. A resulting URL might look like
-    `https://teams.microsoft.com/l/channel/19%3a8451e761b67a4416b47ac034d6d8cc5c%40thread.tacv2/aws-security-hub-test?groupId=048113e8-d452-4921-95dd-be5f410e7aaf&tenantId=35591627-bdde-4d16-a221-bf72ffc20990`
-    and the Channel ID is between the slashes after `channel`, in this case the Channel ID i
-    is `19%3a8451e761b67a4416b47ac034d6d8cc5c%40thread.tacv2`.
     EOF
   type        = string
   default     = ""
@@ -101,47 +80,56 @@ variable "msteams_tenant_id" {
   default     = ""
 }
 
-variable "email_addresses" {
+variable "chatbot_slack_config" {
   description = <<EOF
-    List of email addresses to send notifications to.
-    EOF
-  type        = list(string)
+    A list of configuration objects for Slack channels. See the "Channel Configuration" section
+    in the README for more details.
+
+    The SNS topic names will be prefixed with "apres-alerting-" and postfixed with the name.
+  EOF
   default     = []
+  type = list(object({
+    name                = string
+    publishing_services = list(string)
+    slack_channel_id    = string
+  }))
   validation {
-    condition     = alltrue([for x in var.email_addresses : can(regex("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", x))])
-    error_message = "Email addresses must be valid."
+    condition     = alltrue([for c in var.chatbot_slack_config : length(c.publishing_services) > 0])
+    error_message = "Must specify at least one publishing_service."
+  }
+  validation {
+    condition     = alltrue([for c in var.chatbot_slack_config : c.name != ""])
+    error_message = "Must specify a name."
+  }
+  validation {
+    condition     = alltrue([for c in var.chatbot_slack_config : c.slack_channel_id != ""])
+    error_message = "Must specify a name."
   }
 }
 
-variable "publishing_services" {
+variable "chatbot_msteams_config" {
   description = <<EOF
-    A list of AWS services which are granted access to publish to the SNS topic. The topic is created with encryption
-    enabled, and the services need to be granted access to publish to the topic using the key. For example:
-    ["events.amazonaws.com", "health.amazonaws.com", "config.amazonaws.com", "trustedadvisor.amazonaws.com"]
+    A list of configuration objects for Slack channels. See the "Channel Configuration" section
+    in the README for more details.
 
-    "events.amazonaws.com" is for EventBridge (CloudWatch Events), and "costalerts.amazonaws.com" is for Budget
-    and Cost Anomaly alerts.
+    The SNS topic names will be prefixed with "apres-alerting-" and postfixed with the name.
   EOF
-  type        = list(string)
   default     = []
+  type = list(object({
+    name                = string
+    publishing_services = list(string)
+    msteams_channel_id  = string
+  }))
   validation {
-    condition     = length(var.publishing_services) > 0
-    error_message = "At least one service must be set."
+    condition     = alltrue([for c in var.chatbot_msteams_config : length(c.publishing_services) > 0])
+    error_message = "Must specify at least one publishing_service."
   }
-}
-
-variable "chatbot_policy_arns" {
-  description = <<EOF
-    An AWS IAM Policy ARNs to attach to the Chatbot. The policies should contain the necessary
-    permissions for the Chatbot to interact with the services. For example:
-    ["arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"]
-
-    This policy gives the users in your channels permission to access AWS resources, so take care to
-    limit what is granted.
-
-    If no policy ARN is given, a default empty policy is set, granting only sts:GetCallerIdentity permission.
-
-  EOF
-  type        = list(string)
-  default     = []
+  validation {
+    condition     = alltrue([for c in var.chatbot_msteams_config : c.name != ""])
+    error_message = "Must specify a name."
+  }
+  validation {
+    condition     = alltrue([for c in var.chatbot_msteams_config : c.msteams_channel_id != ""])
+    error_message = "Must specify a name."
+  }
 }
