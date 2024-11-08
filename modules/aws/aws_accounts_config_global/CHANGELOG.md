dd4be50e77620329a3c4f0579b5ada7e82fdc431
Michael Hart <hart.mike@gmail.com>
2024-11-08 10:36:26 -0500

    Add role for Grafana CloudWatch and Lambda access (#275)
    * Add role for Grafana CloudWatch and Lambda Configurator access
    
    * Update aws_accounts_config_global CHANGELOG
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- modules/aws/aws_accounts_config_global/CHANGELOG.md
+++ modules/aws/aws_accounts_config_global/CHANGELOG.md
@@ -2,6 +2,10 @@
 
 This change log is automatically generated.
 
+## 0.2.0 - 2024-11-08
+
+Add role for Grafana cross-account access.
+
 ## 0.1.0 - 2024-07-12
 
 Initial creation of module to manage AWS account global configuration
--- modules/aws/aws_accounts_config_global/main.tf
+++ modules/aws/aws_accounts_config_global/main.tf
@@ -1,3 +1,14 @@
+locals {
+  tags = {
+    application = "Observability"
+    component   = "CloudWatch"
+    environment = "Global"
+    owner       = "Engineering"
+    managed-by  = "Terraform"
+  }
+}
+
+
 # Set a password policy for the account. This matches the two standards:
 # * AWS Foundational Security Best Practices v1.0.0,
 # * CIS AWS Foundations Benchmark v1.2.0
@@ -10,4 +21,74 @@ resource "aws_iam_account_password_policy" "strict" {
   allow_users_to_change_password = true
   max_password_age               = 90
   password_reuse_prevention      = 24
+}
+
+# Add IAM artifacts for Grafana in the monitoring account to access CloudWatch
+# in this account. We don't have a named role for Grafana, so we use the root.
+data "aws_iam_policy_document" "cloudwatch_assume_role" {
+  count = var.monitoring_account_id == "" ? 0 : 1
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${var.monitoring_account_id}:root"]
+    }
+  }
+}
+
+resource "aws_iam_role" "cloudwatch" {
+  count = var.monitoring_account_id == "" ? 0 : 1
+  # Hard coding the name because it needs to be used in the monitoring account
+  name               = "ApresGrafanaCrossAccountAccess"
+  assume_role_policy = data.aws_iam_policy_document.cloudwatch_assume_role[0].json
+  tags = merge(
+    local.tags,
+    {
+      Name = "ApresGrafanaCrossAccountAccess"
+    },
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch" {
+  count      = var.monitoring_account_id == "" ? 0 : 1
+  role       = aws_iam_role.cloudwatch[0].name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess"
+}
+
+# Add IAM artifacts for the Lambda in the monitoring account to access CloudWatch
+# Alarms in this account.
+data "aws_iam_policy_document" "lambda_assume_role" {
+  count = var.monitoring_account_id == "" ? 0 : 1
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${var.monitoring_account_id}:root"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "aws:PrincipalArn"
+      values   = ["arn:aws:iam::${var.monitoring_account_id}:role/ApresGrafanaConfiguratorLambda"]
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda" {
+  count              = var.monitoring_account_id == "" ? 0 : 1
+  name               = "ApresGrafanaConfiguratorCrossAccountAccess"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role[0].json
+  tags = merge(
+    local.tags,
+    {
+      Name = "ApresGrafanaConfiguratorCrossAccountAccess"
+    },
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "lambda" {
+  count      = var.monitoring_account_id == "" ? 0 : 1
+  role       = aws_iam_role.lambda[0].name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess"
 }
