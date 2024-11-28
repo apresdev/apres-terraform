7064d07ee99d3e1a99a4ea44fac37f070d7c80ea
Michael Hart <hart.mike@gmail.com>
2024-11-28 09:20:36 -0500

    Change name of variable to match docs

--- modules/aws/aws_accounts_config_global/variables.tf
+++ modules/aws/aws_accounts_config_global/variables.tf
@@ -1,4 +1,4 @@
-variable "monitoring_account_id" {
+variable "observe_account_id" {
   description = <<EOF
         The AWS account ID of the monitoring account. This account will be granted access to
         view CloudWatch metrics, alarms and logs.
@@ -6,7 +6,7 @@ variable "monitoring_account_id" {
   type        = string
   default     = ""
   validation {
-    condition     = can(regex("^$|^[0-9]{12}$", var.monitoring_account_id))
-    error_message = "monitoring_account_id must either be a 12 digit AWS account ID, or an empty string."
+    condition     = can(regex("^$|^[0-9]{12}$", var.observe_account_id))
+    error_message = "observe_account_id must either be a 12 digit AWS account ID, or an empty string."
   }
 }
