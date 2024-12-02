8cad56a9d668d2aa8038dd4ef65fa6c3035f91e7
Michael Hart <hart.mike@gmail.com>
2024-12-02 17:04:57 +0000

    Bump versions for dealing with race condition

--- modules/aws/aws_accounts_config_workloads/main.tf
+++ modules/aws/aws_accounts_config_workloads/main.tf
@@ -13,21 +13,27 @@ module "messaging_regional" {
 
 module "lambda_regional" {
   #checkov:skip=CKV_TF_1: No hash specified, that's ok because we are using the version.
-  source      = "git@github.com:apresdev/apres-terraform.git//modules/aws/lambda_regional?ref=rel/lambda_regional/0.3.0"
+  source      = "git@github.com:apresdev/apres-terraform.git//modules/aws/lambda_regional?ref=rel/lambda_regional/0.3.1"
   environment = "WorkloadConfig"
 }
 
 module "ecs_events" {
   #checkov:skip=CKV_TF_1: No hash specified, that's ok because we are using the version.
-  source      = "git@github.com:apresdev/apres-terraform.git//modules/aws/ecs_events?ref=rel/ecs_events/0.3.0"
+  source      = "git@github.com:apresdev/apres-terraform.git//modules/aws/ecs_events?ref=rel/ecs_events/0.3.1"
   name        = "ECSEvents"
   environment = "WorkloadConfig"
   application = "ECSEvents"
   component   = "ECSEvents"
 
   # Need to pass this in or else we get a race condition with lookups and creations on new accounts
-  code_signing_profile_name = module.lambda_regional.signing_profile_name
-  code_signing_config_arn   = module.lambda_regional.signing_config_arn
+  code_signing_name_ssm_parameter = module.lambda_regional.signing_config_name_ssm_parameter
+  code_signing_arn_ssm_parameter  = module.lambda_regional.signing_config_arn_ssm_parameter
+
+  # Need this to handel the race condition with the ssm parameters that don't exist yet on
+  # new accounts.
+  depends_on = [
+    module.lambda_regional
+  ]
 }
 
 resource "aws_ebs_encryption_by_default" "default" {
