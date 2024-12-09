f46f1c3709a894729550fdd119e63191838f592b
Michael Hart <hart.mike@gmail.com>
2024-12-09 17:52:06 +0000

    Fix outputs bug

--- modules/aws/alerting/outputs.tf
+++ modules/aws/alerting/outputs.tf
@@ -1,4 +1,4 @@
 output "sns_topic_arns" {
   description = "List of ARNs for the SNS topics created for alerting."
-  value       = [for topic in aws_sns_topic.default : topic.value.arn]
+  value       = [for topic in aws_sns_topic.default : topic.arn]
 }
