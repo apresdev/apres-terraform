7e3af4b83b5370a30b478554c83cafafbf0db8e1
Michael Hart <hart.mike@gmail.com>
2024-12-13 13:24:22 -0500

    Enable multiple instances in an AWS account. (#319)
    * Enable multiple instances in an AWS account.
    
    * Update vpc CHANGELOG
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- modules/aws/vpc/outputs.tf
+++ modules/aws/vpc/outputs.tf
@@ -3,11 +3,6 @@ output "vpc_id" {
   value       = aws_vpc.vpc.id
 }
 
-output "nat_dashboard_url" {
-  description = "URL for the NAT Instance Dashboard"
-  value       = "https://${data.aws_region.current.name}.console.aws.amazon.com/cloudwatch/home#dashboards:name=${var.nat_instance_dashboard_name}"
-}
-
 output "private_subnet_ids" {
   description = "List of Private Subnet IDs"
   value       = aws_subnet.private_subnet[*].id
