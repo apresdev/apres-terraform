528149e45ddcffd645cf470dbd474bbd72e30f63
Michael Hart <hart.mike@gmail.com>
2024-12-23 18:04:26 +0000

    Fix GH Actions defaults

--- modules/github/github-manager/actions.tf
+++ modules/github/github-manager/actions.tf
@@ -2,16 +2,16 @@
 resource "github_actions_repository_permissions" "managed_repository" {
   for_each        = local.repo_definitions
   repository      = each.key
-  enabled         = each.value.actions.enabled
-  allowed_actions = each.value.actions.allowed_actions
+  enabled         = try(each.value.actions.enabled, local.defaults.actions.enabled)
+  allowed_actions = try(each.value.actions.allowed_actions, local.defaults.actions.allowed_actions)
 
   dynamic "allowed_actions_config" {
     # Only create this section if the allowed_actions is set to selected, else it'll fail.
-    for_each = each.value.actions.allowed_actions == "selected" ? [1] : []
+    for_each = try(each.value.actions.allowed_actions == "selected" ? [1] : [], [])
     content {
-      github_owned_allowed = each.value.actions.allowed_select_github_owned
-      verified_allowed     = each.value.actions.allowed_select_verified
-      patterns_allowed     = each.value.actions.allowed_select_patterns
+      github_owned_allowed = try(each.value.actions.allowed_select_github_owned, local.defaults.actions.allowed_select_github_owned)
+      verified_allowed     = try(each.value.actions.allowed_select_verified, local.defaults.actions.allowed_select_verified)
+      patterns_allowed     = try(each.value.actions.allowed_select_patterns, local.dedicated_actions.allowed_select_patterns)
     }
   }
 }
@@ -20,7 +20,7 @@ resource "github_actions_repository_permissions" "managed_repository" {
 resource "github_actions_repository_access_level" "managed_repository" {
   for_each     = local.repo_definitions
   repository   = each.key
-  access_level = each.value.actions.reusable_actions_scope
+  access_level = try(each.value.actions.reusable_actions_scope, local.defaults.actions.reusable_actions_scope)
 
   depends_on = [
     github_repository.managed_repository
