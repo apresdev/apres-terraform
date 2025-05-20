a0f8d6d40f12cb9687c5ef656891ec2241294162
Michael Hart <hart.mike@gmail.com>
2025-05-20 12:25:11 -0400

    Update module dependencies (#425)
    * Update module dependencies
    
    * Update grafana_managed CHANGELOG
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- modules/aws/grafana_managed/lambda.tf
+++ modules/aws/grafana_managed/lambda.tf
@@ -85,7 +85,7 @@ resource "aws_iam_role_policy" "lambda" {
 
 module "lambda" {
   #checkov:skip=CKV_TF_1:False positive, we are not using a hash because we use the tagged version.
-  source = "git@github.com:apresdev/apres-terraform.git//modules/aws/lambda?ref=rel/lambda/1.0.0"
+  source = "git@github.com:apresdev/apres-terraform.git//modules/aws/lambda?ref=rel/lambda/1.1.2"
 
   name        = "Grafana"
   environment = var.environment
