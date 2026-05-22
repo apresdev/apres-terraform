25630329b6e7c6406ad5897ef6a5921f356875fa
Michael Hart <hart.mike@gmail.com>
2026-05-22 12:50:09 -0400

    Bump security_tools to 1.2.3

--- modules/aws/security_tools/alerting.tf
+++ modules/aws/security_tools/alerting.tf
@@ -1,7 +1,7 @@
 module "alerting" {
   # Use remote source so we can keep versioning correctly, even though the module is in the same repo.
   #checkov:skip=CKV_TF_1: Explicitly using versions, not a hash.
-  source              = "git::https://github.com/apresdev/apres-terraform.git//modules/aws/alerting?ref=rel/alerting/2.1.2"
+  source              = "git::https://github.com/apresdev/apres-terraform.git//modules/aws/alerting?ref=rel/alerting/2.1.3"
   name                = "securityhub"
   publishing_services = ["events.amazonaws.com"]
   environment         = var.environment
