89d6877239ca79e780fd92cd05ef2173586c297a
Michael Hart <hart.mike@gmail.com>
2026-05-22 13:12:15 -0400

    Bump cloudfront_s3 to 1.6.4

--- modules/aws/cloudfront_s3/waf.tf
+++ modules/aws/cloudfront_s3/waf.tf
@@ -1,7 +1,7 @@
 module "waf" {
   count = var.waf_arn == "" ? 1 : 0
   #checkov:skip=CKV_TF_1: No hash specified, that's ok because we are using the version.
-  source      = "git::https://github.com/apresdev/apres-terraform.git//modules/aws/waf?ref=rel/waf/1.1.2"
+  source      = "git::https://github.com/apresdev/apres-terraform.git//modules/aws/waf?ref=rel/waf/1.1.3"
   name        = var.name
   environment = var.environment
   application = var.application
